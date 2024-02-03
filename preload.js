29de8abea188a3f118e60912f84453a30dc60c0a
Karolina Redden <dubajkaro@gmail.com>
2024-02-03 17:13:41 -0400

    add a source

--- preload.js
+++ preload.js
@@ -1,3 +1,4 @@
+//https://www.electronjs.org/docs/latest/tutorial/quick-start#access-nodejs-from-the-renderer-with-a-preload-script
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector);
