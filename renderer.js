aaba1c34b371b2883825d0419ed523ea6c24d4bd
Karolina Redden <dubajkaro@gmail.com>
2024-02-14 18:47:24 -0400

    load video to the player, assign the src tag

--- renderer.js
+++ renderer.js
@@ -1,23 +1,23 @@
 // const { ipcRenderer } = require('electron');
-// const fs = require("fs")
-
 
 //https://www.electronjs.org/docs/latest/tutorial/quick-start#access-nodejs-from-the-renderer-with-a-preload-script
-// window.addEventListener('DOMContentLoaded', () => {
-// const replaceText = (selector, text) => {
-//   const element = document.getElementById(selector);
-//   if (element) element.innerText = text
-// }
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector);
+    if (element) element.innerText = text
+  }
 
-// for (const dependency of ['chrome', 'node', 'electron']) {
-//   replaceText(`${dependency}-version`, process.versions[dependency])
-// }
-// })
-
-const { ipcRenderer } = require('electron');
+  for (const dependency of ['chrome', 'node', 'electron']) {
+    replaceText(`${dependency}-version`, process.versions[dependency])
+  }
+})
 
 ipcRenderer.on('video-selected', (event, path) => {
   console.log(path)
+  console.log(path.split('/').pop())
+  const videoSrc = path.split('/').pop();
+  document.querySelector('#videoSource').src = videoSrc;
+  document.querySelector(".js-player").load();
 })
 
 
